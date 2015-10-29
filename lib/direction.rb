6ef0886b9847481a996f6f83c83d17775e8e837d
Michael Treacher <m.w.treacher@gmail.com>
2015-10-29 21:00:51 +1100

    Add byebug for debugging. Implemented left and right for the direction class.

--- lib/direction.rb
+++ lib/direction.rb
@@ -1,7 +1,33 @@
 class Direction
   attr_reader :value
-  
+
+  COMPASS = [:west, :north, :east, :south].freeze
+  LEFT    = -1
+  RIGHT   = 1
+
   def initialize(value)
     @value = value
+  end
+
+  def left_direction
+    direction_for(LEFT)
+  end
+
+  def right_direction
+    direction_for(RIGHT)
+  end
+
+  def eql?(object)
+    self.value == object.value
+  end
+
+  private
+
+  def direction_for(turn)
+    Direction.new(compass_for_direction.rotate(turn).first).freeze
+  end
+
+  def compass_for_direction
+    COMPASS.rotate(COMPASS.index(value))
   end
 end
--- spec/direction_spec.rb
+++ spec/direction_spec.rb
@@ -1,11 +1,88 @@
 require 'direction'
 
 describe Direction do
-  let(:direction) { Direction.new(:east) }
+  let(:value)     { :east }
+  let(:direction) { Direction.new(value) }
 
-  context 'attributes' do
+  context "attributes" do
     it "has a value of east" do
       expect(direction.value).to eq :east
+    end
+  end
+
+  describe "#left_direction" do
+    context "direction is north" do
+      let(:value)              { :north }
+      let(:expected_direction) { Direction.new(:west) }
+
+      it "returns a west direction object" do
+        expect(direction.left_direction).to eql expected_direction
+      end
+    end
+
+    context "direction is east" do
+      let(:value)              { :east }
+      let(:expected_direction) { Direction.new(:north) }
+
+      it "returns a north direction object" do
+        expect(direction.left_direction).to eql expected_direction
+      end
+    end
+
+    context "direction is south" do
+      let(:value)              { :south }
+      let(:expected_direction) { Direction.new(:east) }
+
+      it "returns a east direction object" do
+        expect(direction.left_direction).to eql expected_direction
+      end
+    end
+
+    context "direction is west" do
+      let(:value)              { :west }
+      let(:expected_direction) { Direction.new(:south) }
+
+      it "returns a south direction object" do
+        expect(direction.left_direction).to eql expected_direction
+      end
+    end
+  end
+
+  describe "#right_direction" do
+    context "direction is north" do
+      let(:value)              { :north }
+      let(:expected_direction) { Direction.new(:east) }
+
+      it "returns a east direction object" do
+        expect(direction.right_direction).to eql expected_direction
+      end
+    end
+
+    context "direction is east" do
+      let(:value)              { :east }
+      let(:expected_direction) { Direction.new(:south) }
+
+      it "returns a south direction object" do
+        expect(direction.right_direction).to eql expected_direction
+      end
+    end
+
+    context "direction is south" do
+      let(:value)              { :south }
+      let(:expected_direction) { Direction.new(:west) }
+
+      it "returns a west direction object" do
+        expect(direction.right_direction).to eql expected_direction
+      end
+    end
+
+    context "direction is west" do
+      let(:value)              { :west }
+      let(:expected_direction) { Direction.new(:north) }
+
+      it "returns a north direction object" do
+        expect(direction.right_direction).to eql expected_direction
+      end
     end
   end
 end
