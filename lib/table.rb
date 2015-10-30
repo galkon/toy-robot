b84fcf9667006b67978a42c1f76e35a650513b8f
Michael Treacher <m.w.treacher@gmail.com>
2015-10-30 21:33:39 +1100

    Added table class with the ability to place a robot

---
+++ lib/table.rb
@@ -0,0 +1,19 @@
+class Table
+  attr_reader :grid
+
+  def initialize
+    @grid = Array.new(5) { Array.new(5) }
+  end
+
+  def place_robot(robot)
+    remove_robots
+    @grid[robot.position.y][robot.position.x] = robot if robot.position.y < @grid.size
+    self
+  end
+
+  private
+
+  def remove_robots
+    @grid.each { |row| row.delete_if { |item| item.is_a?(Robot) } }
+  end
+end
